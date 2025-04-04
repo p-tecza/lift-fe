141ed121971fbdb8118155d26c4fc4d23d4ccd2b
p-tecza <pawel.tecza.pt@gmail.com>
2025-04-05 00:14:00 +0200

    new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {LandingPageComponent} from "./lift/landing-page/landing-page.component";
 import {UploadDocComponent} from "./lift/upload-doc/upload-doc.component";
+import {AnalyzePanelComponent} from "./lift/analyze-panel/analyze-panel.component";
 
 export const routes: Routes = [
   {path: 'dashboard', component: LandingPageComponent},
-  {path: 'upload', component: UploadDocComponent}
+  {path: 'upload', component: UploadDocComponent},
+  {path: 'analyze', component: AnalyzePanelComponent}
 ];
