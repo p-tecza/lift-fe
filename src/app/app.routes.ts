a2a3a203eafc2b8636756cd72bb85618f7afdcc2
p-tecza <pawel.tecza.pt@gmail.com>
2025-04-06 19:13:29 +0200

    fixx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import {LandingPageComponent} from "./lift/components/landing-page/landing-page.component";
-import {UploadDocComponent} from "./lift/upload-doc/upload-doc.component";
 import {AnalyzePanelComponent} from "./lift/components/analyze-panel/analyze-panel.component";
 import {AccountPanelComponent} from "./lift/components/account-panel/account-panel.component";
+import {UploadDocComponent} from "./lift/components/upload-doc/upload-doc.component";
 
 export const routes: Routes = [
   {path: 'dashboard', component: LandingPageComponent},
