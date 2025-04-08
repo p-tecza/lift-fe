c790ab9ea92da808c5b03338f5ba8771c0316f7b
p-tecza <pawel.tecza.pt@gmail.com>
2025-04-09 00:26:10 +0200

    file upload view

--- src/app/lift/components/upload-doc/file-uploader/file-uploader.component.ts
+++ src/app/lift/components/upload-doc/file-uploader/file-uploader.component.ts
@@ -5,6 +5,7 @@ import {MatProgressBar} from "@angular/material/progress-bar";
 import {finalize, Subscription} from "rxjs";
 import {MatMiniFabButton} from "@angular/material/button";
 import {NgIf} from "@angular/common";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 
 @Component({
@@ -14,7 +15,8 @@ import {NgIf} from "@angular/common";
     MatIcon,
     MatProgressBar,
     MatMiniFabButton,
-    NgIf
+    NgIf,
+    MatProgressSpinner
   ],
   templateUrl: './file-uploader.component.html',
   styleUrl: './file-uploader.component.scss'
@@ -25,34 +27,42 @@ export class FileUploaderComponent {
   @Input()
   requiredFileType:string[] | undefined;
 
-  fileName = '';
+  fileNames:string[] = ["TEST.png","ROZWALAJACY_CSS_TEST_EOOEOEOEOE.pdf","ea23.jpg","ROZWALAJACY_CSS_TEST_EOOEOEOEOE.pdf","ea23.jpg"
+    ,"ROZWALAJACY_CSS_TEST_EOOEOEOEOE.pdf","ea23.jpg","ROZWALAJACY_CSS_TEST_EOOEOEOEOE.pdf","ea23.jpg","ROZWALAJACY_CSS_TEST_EOOEOEOEOE.pdf","ea23.jpg"];
+  files: File[] = [];
   uploadProgress:number | undefined = undefined;
   uploadSub: Subscription | undefined = undefined;
 
   constructor() {} //private http: HttpClient
 
   onFileSelected(event: any) {
-    const file:File = event.target.files[0];
+    const files:FileList = event.target.files;
+    this.fileNames = [];
+    console.log("ee");
+    console.log(files)
+    Array.from(files).forEach((file) => {
+      if (file) {
+        this.fileNames.push(file.name);
+        this.files.push(file);
+        const formData = new FormData();
+        formData.append("thumbnail", file);
+        this.uploadProgress = 1;
+        // const upload$ = this.http.post("/api/thumbnail-upload", formData, {
+        //   reportProgress: true,
+        //   observe: 'events'
+        // })
+        //   .pipe(
+        //     finalize(() => this.reset())
+        //   );
 
-    if (file) {
-      this.fileName = file.name;
-      const formData = new FormData();
-      formData.append("thumbnail", file);
-      this.uploadProgress = 1;
-      // const upload$ = this.http.post("/api/thumbnail-upload", formData, {
-      //   reportProgress: true,
-      //   observe: 'events'
-      // })
-      //   .pipe(
-      //     finalize(() => this.reset())
-      //   );
-
-      // this.uploadSub = upload$.subscribe(event => {
-      //   if (event.type == HttpEventType.UploadProgress && event.total) {
-      //     this.uploadProgress = Math.round(100 * (event.loaded / event.total));
-      //   }
-      // })
-    }
+        // this.uploadSub = upload$.subscribe(event => {
+        //   if (event.type == HttpEventType.UploadProgress && event.total) {
+        //     this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+        //   }
+        // })
+        console.log(this.fileNames);
+      }
+    })
   }
 
   cancelUpload() {
@@ -64,6 +74,11 @@ export class FileUploaderComponent {
   reset() {
     this.uploadProgress = undefined;
     this.uploadSub = undefined;
+  }
+
+  clearFiles(){
+    this.fileNames = [];
+    this.files = [];
   }
 
 }
